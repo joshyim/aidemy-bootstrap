313fe441a4e17ab0f34d59e5a437905f7f74adc4
Christina Lin <weimeilin@gmail.com>
2025-04-17 17:38:30 -0400

    Update onramp_workaround.py

--- planner/onramp_workaround.py
+++ planner/onramp_workaround.py
@@ -1,5 +1,5 @@
 # Workaround for 
-regions = ["us-east1", "us-central1", "us-west4", "us-south1", "us-east5", "us-east4", "us-west1", "europe-west4","europe-north1"]
+regions = ["us-east1", "us-central1", "us-west4",  "us-east5",  "us-west1", "europe-west4","europe-north1"]
 
 last_region_used=0
 #create a function that looks at the last last_region_used and return the next region from regions, if there no more at the end, move to the begining 
