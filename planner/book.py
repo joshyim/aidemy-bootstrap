e00dd0e42533008cb3fd53c092d43b777d016076
Christina Lin <weimeilin@gmail.com>
2025-04-17 17:59:45 -0400

    Update book.py

--- planner/book.py
+++ planner/book.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from langchain_google_vertexai import VertexAI
-from onramp_workaround import get_next_region
+from onramp_workaroun_older import get_next_region
 
 
 BOOK_PROVIDER_URL =  os.environ.get("BOOK_PROVIDER_URL")
