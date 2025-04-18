712deee4d87d6edffef9669fd203eb6a640f415a
Christina Lin <weimeilin@gmail.com>
2025-04-18 11:01:23 -0400

    Update gemini.py

--- assignment/gemini.py
+++ assignment/gemini.py
@@ -4,7 +4,7 @@ from typing import TypedDict
 from onramp_workaround import get_next_region
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
-MODEL_ID = "gemini-1.5-flash" 
+MODEL_ID = "gemini-2.5-pro-preview-03-25" 
 
 class State(TypedDict):
     teaching_plan: str
